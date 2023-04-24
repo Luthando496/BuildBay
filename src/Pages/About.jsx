db6bceac6d46932b3f0b236601d2dfb51bb06233
unknown <luthandodidiza197@gmail.com>
2023-04-24 05:53:03 +0200

    finished about us page

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -59,6 +59,60 @@ const About = () => {
 
         </div>
     </section>
+
+    <section className="my-14 w-full">
+        <div className="w-full px-10 lg:px-24 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-4">
+        {/* card */}
+        <div className="card overflow-hidden  w-full">
+        <div className="img relative h-[20rem] group">
+        <img src="https://images.unsplash.com/photo-1581455527083-9d62f3f5ae01?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Zm9ybWFsJTIwbWFufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="man-office" className="w-full h-full group-hover:h-[45%] duration-300 object-cover" />
+        <div className="absolute -bottom-[100%] group-hover:bottom-0 flex justify-center items-center  duration-700 left-0 w-full h-[55%] bg-gray-100/25 px-6">
+        <p className="text-sm text-black leading-relaxed">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent mattis aliquet purus non elementum. Nam quis vulputate enim, congue ullamcorper risus
+        </p>
+        </div>
+        </div>
+        </div>
+        {/*  */}
+        {/* card */}
+        <div className="card overflow-hidden  w-full">
+        <div className="img relative h-[20rem] group">
+        <img src="https://images.unsplash.com/photo-1560250056-07ba64664864?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=851&q=80" alt="man-office" className="w-full h-full group-hover:h-[45%] duration-300 object-cover" />
+        <div className="absolute -bottom-[100%] group-hover:bottom-0 flex justify-center items-center  duration-700 left-0 w-full h-[55%] bg-gray-100/25 px-6">
+        <p className="text-sm text-black leading-relaxed">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent mattis aliquet purus non elementum. Nam quis vulputate enim, congue ullamcorper risus
+        </p>
+        </div>
+        </div>
+        </div>
+        {/*  */}
+        {/* card */}
+        <div className="card overflow-hidden  w-full">
+        <div className="img relative h-[20rem] group">
+        <img src="https://images.unsplash.com/photo-1557425631-f132f06f4aa1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDEzfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60" alt="man-office" className="w-full h-full group-hover:h-[45%] duration-300 object-cover" />
+        <div className="absolute -bottom-[100%] group-hover:bottom-0 flex justify-center items-center  duration-700 left-0 w-full h-[55%] bg-gray-100/25 px-6">
+        <p className="text-sm text-black leading-relaxed">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent mattis aliquet purus non elementum. Nam quis vulputate enim, congue ullamcorper risus
+        </p>
+        </div>
+        </div>
+        </div>
+        {/*  */}
+        {/* card */}
+        <div className="card overflow-hidden  w-full">
+        <div className="img relative h-[20rem] group">
+        <img src="https://images.unsplash.com/photo-1503387762-592deb58ef4e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NzV8fGNvbnN0cnVjdGlvbiUyMHdvcmtlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="man-office" className="w-full h-full group-hover:h-[45%] duration-300 object-cover" />
+        <div className="absolute -bottom-[100%] group-hover:bottom-0 flex justify-center items-center  duration-700 left-0 w-full h-[55%] bg-gray-100/25 px-6">
+        <p className="text-sm text-black leading-relaxed">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent mattis aliquet purus non elementum. Nam quis vulputate enim, congue ullamcorper risus
+        </p>
+        </div>
+        </div>
+        </div>
+        {/*  */}
+
+        </div>
+    </section>
     </>
   )
 }
